8998bbaae86c6670348faadaba8b6eca7dad628d
Austin Hansen <austinthansen@gmail.com>
2014-06-21 13:55:34 -0600

    added cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'austinhansen'
+  config.api_key = '792295966462899'
+  config.api_secret = 'mHdgl15na7QQ0lgq9sk3vb2eGeo'
+  config.cdn_subdomain = true
+end
